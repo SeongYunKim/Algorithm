ea439317f198213b7505c48852efe8e15f7c94a3
SeongYunKim <cauboy17@naver.com>
2020-04-15 16:53:13 +0900

    2020-04-15

--- Backtracking/17370(육각형 우리 속의 개미).cpp
+++ Backtracking/17370(육각형 우리 속의 개미).cpp
@@ -1,42 +1,48 @@
+/*
+날짜: 2020-04-16
+분류: Backtracking
+TIP: 이미 방문했던 점을 또 방문할 때 cnt가 N-1이면 result에 1 추가
+TIP: */
+
 #include <iostream>
 #pragma warning (disable: 4996)
 
 using namespace std;
 
-int N,  result = 0;
+int N, result = 0;
 int visited[3000][3000] = { 0 };
-int move_x[6] = { 23, -23, 1, 1, -1, -1 };
-int move_y[6] = { 0, 0, 1, -1, 1, -1 };
+int move_x[6] = { 0, 0, 1, 1, -1, -1 };
+int move_y[6] = { 23, -23, 1, -1, 1, -1 };
 int match[6][2] = { {2, 4}, {3, 5}, {0, 3}, {1, 2}, {0, 5}, {1, 4} };
 
 void dfs(int cnt, int cur_x, int cur_y, int cur_dir);
 
 int main() {
 	scanf("%d", &N);
-	visited[477][500] = 1;
+	visited[500][477] = 1;
+	visited[500][500] = 1;
 	dfs(0, 500, 500, 0);
 	printf("%d", result);
 	return 0;
 }
 
 void dfs(int cnt, int cur_x, int cur_y, int cur_dir) {
-	printf("%d: %d %d %d\n", cnt, cur_x, cur_y, cur_dir);
 	if (cnt == N) {
-		if (cur_x == 477 && cur_y == 500) {
-			printf("adflakjs;\n");
-		}
-		printf("\n");
-		if (visited[cur_x][cur_y] > 1) {
-			result++;
-		}
 		return;
 	}
+
 	int next_x, next_y;
 	for (int i = 0; i < 2; i++) {
 		next_x = cur_x + move_x[match[cur_dir][i]];
 		next_y = cur_y + move_y[match[cur_dir][i]];
-		visited[next_x][next_y]++;
-		dfs(cnt + 1, next_x, next_y, match[cur_dir][i]);
-		visited[next_x][next_y]--;
+		if (visited[next_x][next_y] == 0) {
+			visited[next_x][next_y]++;
+			dfs(cnt + 1, next_x, next_y, match[cur_dir][i]);
+			visited[next_x][next_y]--;
+		}
+		else {
+			if (cnt == N - 1)
+				result++;
+		}
 	}
 }
